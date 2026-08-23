ac618a19e6d88e59e35dd77590403775ac7ca358
Dheer46 <59868792+Dheer46@users.noreply.github.com>
2026-08-24 01:27:21 +0530

    Time: 0 ms (100%), Space: 9 MB (19.29%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        string ans;
+
+        for(int i = k-1;i>=0;i--){
+            ans.push_back(s[i]);
+        }
+        for(int i = k ;i<s.size();i++){
+            ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
