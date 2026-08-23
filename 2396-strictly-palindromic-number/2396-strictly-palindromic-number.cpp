e84dab6016b5eb59c86b26191c0cc65ce849d5fb
Dheer46 <59868792+Dheer46@users.noreply.github.com>
2026-08-24 01:12:58 +0530

    Time: 0 ms (100%), Space: 7.9 MB (28.18%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        return false;
+        for(int base = 2; base <= n-2; base++){
+            int num = n;
+            vector<int> digits;
+            while(num>0){
+                digits.push_back(num % base);
+                num = num/base;
+            }
+            int i = 0;
+            int j = digits.size()-1;
+            while(i<j){
+                if(digits[i]!=digits[j]){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+        }
+        return true;
     }
 };
